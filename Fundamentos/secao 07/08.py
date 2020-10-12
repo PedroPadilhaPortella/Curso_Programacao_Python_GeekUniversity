b0cf25798e98bb3633744383d994ef7aa4491ecd
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-12 00:24:27 -0300

    exercicios

--- Fundamentos/secao 07/08.py
+++ Fundamentos/secao 07/08.py
@@ -6,3 +6,6 @@ for i in range(0, 6):
     array.append(randint(1, 99))
 print(array)
 print(array[::-1])
+
+array.reverse()
+print(array)
--- Fundamentos/secao 07/15.py
+++ Fundamentos/secao 07/15.py
@@ -1,3 +1,7 @@
 lista = [5, 2, 6, 1, 9]
-lista.reverse()
-print(lista)
+# lista.reverse()
+print(lista[::-1])
+
+lista2 = []
+lista2.copy(lista)
+print(lista2)
