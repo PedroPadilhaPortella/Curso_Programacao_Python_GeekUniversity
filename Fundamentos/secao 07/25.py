8687af2a9829f503f09dfe7e5e47df2ecd8936df
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-23 00:06:40 -0300

    Update 25.py

--- Fundamentos/secao 07/25.py
+++ Fundamentos/secao 07/25.py
@@ -2,10 +2,12 @@ lista = []
 c = 0
 i = 0
 while(c < 100):
-    if((i % 7 == 0) or ((str(i))[-1]) == '7'):
+    i += 1
+    if((str(i))[-1] == '7'):
+        continue
+    if(i % 7 == 0):
         continue
     lista.append(i)
-    i += 1
     c += 1
     if(c == 100):
         break
