3d2d939dec7f2e6690b6718141bcd6414d270836
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-05-23 01:27:35 -0300

    redes neurais calculus

--- Projetos/Redes Neurais/aula2.py
+++ Projetos/Redes Neurais/aula2.py
@@ -1,15 +1,17 @@
 '''
-    Perceptron
+    Bias
 '''
 
 from random import *
 
 pesoInicial1 = random()
 pesoInicial2 = random()
+pesoBias = random()
 
 entrada1 = int(input('Digite a primeira entrada: '))
 entrada2 = int(input('Digite a segunda entrada: '))
 
+bias = 1
 erro = 1
 
 while(erro != 0):
@@ -20,19 +22,21 @@ while(erro != 0):
 
     somatoria = pesoInicial1 * entrada1
     somatoria += pesoInicial2 * entrada2
+    somatoria += pesoBias * bias
 
     if(somatoria < 0):
         resultado = 0
     elif(somatoria >= 0):
         resultado = 1
 
-
     erro = resultadoEsperado - resultado
 
-    print(f"resultado = {resultado}")
     print(f"peso inicial 1 = {pesoInicial1}")
     print(f"peso inicial 2 = {pesoInicial2}")
+    print(f"peso BIAS = {pesoInicial2}")
+    print(f"resultado = {resultado}")
     print(f'erro = {erro}\n')
 
     pesoInicial1 += 0.1 * entrada1 * erro
     pesoInicial2 += 0.1 * entrada2 * erro
+    pesoBias += 0.1 * bias * erro
---
+++ Projetos/Redes Neurais/aula3.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+w0 = 0.3
+w1 = 0.3
+
+def hipotese(x, w0, w1):
+    return w0 + w1 * x
+
+print(hipotese(2, w0, w1))
