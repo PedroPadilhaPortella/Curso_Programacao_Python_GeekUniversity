75ae8cef41cbf5a7dc925fe7ad2cd2b4dde5c919
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-05-26 00:19:09 -0300

    redes neurais hipotese

--- Projetos/Redes Neurais/aula3.py
+++ Projetos/Redes Neurais/aula3.py
@@ -1,3 +1,7 @@
+'''
+    Hipótese
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
---
+++ Projetos/Redes Neurais/aula4.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+w0 = 0.3
+w1 = 0.3
+
+def hipotese(x, w0, w1):
+    return w0 + w1 * x
+
+hipotese(1, w0, w1)
+
+
+x = np.arange(-10, 10, step=1)
+
+h = hipotese(x, w0, w1)
+
+lenh = np.arange(0, 20)
+
+pontosY = [4, 5, 5]
+pontosX = [5, 3, 4]
+
+plt.scatter(pontosY, pontosX)
+plt.title(' Redes Multicamadas - Hipótese')
+plt.grid(True)
+plt.plot(lenh, h)
+plt.show()
