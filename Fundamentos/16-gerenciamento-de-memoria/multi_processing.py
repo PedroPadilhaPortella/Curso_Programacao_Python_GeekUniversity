9ef2f64c8c87533a14777e3c355fa2af9d7af515
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-08-28 23:20:01 -0300

    gerenciamento de memoria em python

---
+++ Fundamentos/16-gerenciamento-de-memoria/multi_processing.py
@@ -0,0 +1,29 @@
+'''
+    Multi Processing
+'''
+
+import time
+from multiprocessing import Pool
+
+CONTADOR = 50000000
+
+def contagem_regressiva(n):
+    while n > 0:
+        n -= 1
+
+
+if __name__ == "__main__":
+    pool = Pool(processes=2)
+    
+    inicio = time.time()
+    
+    r1 = pool.apply_async(contagem_regressiva, [CONTADOR//2])
+    r2 = pool.apply_async(contagem_regressiva, [CONTADOR//2])
+
+    pool.close()
+    pool.join()
+
+    fim = time.time()
+
+    print(f"Tempo em segundos: {fim - inicio}")
+    # Tempo em segundos: 7.3953070640563965
---
+++ Fundamentos/16-gerenciamento-de-memoria/single_thread.py
@@ -0,0 +1,19 @@
+'''
+    Single Thread
+'''
+
+import time
+
+CONTADOR = 50000000
+
+def contagem_regressiva(n):
+    while n > 0:
+        n -= 1
+
+inicio = time.time()
+contagem_regressiva(CONTADOR)
+fim = time.time()
+
+
+print(f"Tempo em segundos: ${fim - inicio}")
+# Tempo em segundos: 12.236196041107178
